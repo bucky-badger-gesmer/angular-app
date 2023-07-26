ef2f57a01fc80e12c15c1f16191488548041c7e4
bucky-badger-gesmer <aaron.gesmer@gmail.com>
2023-07-26 10:02:52 -0500

    lesson 7: interpolation

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -6,7 +6,21 @@ import { HousingLocation } from '../housinglocation';
   selector: 'app-housing-location',
   standalone: true,
   imports: [CommonModule],
-  template: `<p>housing-location works!</p>`,
+  template: `
+    <section class="bg-sky-100 rounded-3xl pb-8">
+      <img
+        class="h-64 w-full object-cover rounded-t-3xl"
+        [src]="housingLocation.photo"
+        alt="Exterior photo of {{ housingLocation.name }}"
+      />
+      <h2 class="text-2xl font-bold text-sky-500 pt-2.5 pb-0 px-5">
+        {{ housingLocation.name }}
+      </h2>
+      <p class="pt-2.5 pb-10 px-10 before:content-locationPin">
+        {{ housingLocation.city }}, {{ housingLocation.state }}
+      </p>
+    </section>
+  `,
 })
 export class HousingLocationComponent {
   @Input() housingLocation!: HousingLocation;
